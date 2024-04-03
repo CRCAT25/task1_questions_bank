6703531b91dc56a46e6b6ca59a15104ab695d08d
doquocthanh <doquocthanh@github.com>
2024-04-03 18:18:01 +0700

    DanhGiaNangLuc - index.js - Add: Nhóm department cho các position

--- task2_danhgianangluc/index.js
+++ task2_danhgianangluc/index.js
@@ -262,6 +262,28 @@ const datas = [
         "CreateTime": "2024-03-29T16:58:06.39",
         "LastModifiedBy": null,
         "LastModifiedTime": null
+    },
+    {
+        "PositionID": "ABCS",
+        "PositionName": "Nhân viên nhà hàng",
+        "DepartmentName": "Hội đồng thành viên",
+        "CompetenceID": "test3",
+        "CompetenceName": "năng lực 3",
+        "CategoryName": "Năng lực marketing",
+        "DepartmentID": "BOM",
+        "CategoryID": "MA",
+        "Category": 5,
+        "Department": 1,
+        "Code": 10691,
+        "Framework": 10103,
+        "Competence": 8,
+        "Position": 53,
+        "CompetenceLevel": null,
+        "CompetenceLevelMax": null,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:58:06.39",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
     }
 ]
 
@@ -269,29 +291,55 @@ const datas = [
 const listCompetence = [];
 const listPosition = [];
 
-// Lấy danh sách các Competence
+// Lấy danh sách các Competence và sắp xếp chung theo categoryID
 const getListCompetence = () => {
     datas.forEach(data => {
         const existingCompetence = listCompetence.find(item => item.id === data.CompetenceID);
         if (!existingCompetence) {
             listCompetence.push({
+                categoryID: data.CategoryID,
                 id: data.CompetenceID,
                 name: data.CompetenceName
             });
         }
     });
+
+    // Sắp xếp danh sách theo categoryID
+    listCompetence.sort((a, b) => {
+        // So sánh categoryID của 2 phần tử a và b
+        if (a.CategoryID < b.CategoryID) {
+            return -1;
+        }
+        if (a.CategoryID > b.CategoryID) {
+            return 1;
+        }
+        return 0;
+    });
 }
 
-// Lấy danh sách các Position
+// Lấy danh sách các Position và sắp xếp chung theo departmentID
 const getListPosition = () => {
     datas.forEach(data => {
-        const existingPosition = listPosition.find(item => item.id === data.PositionID);
+        const existingPosition = listPosition.find(item => item.positionName === data.PositionName);
         if (!existingPosition) {
             listPosition.push({
-                id: data.PositionID,
-                name: data.PositionName
+                departmentID: data.DepartmentID,
+                positionID: data.PositionID,
+                positionName: data.PositionName
             });
         }
+    });
+
+    // Sắp xếp danh sách theo departmentID
+    listPosition.sort((a, b) => {
+        // So sánh departmentID của 2 phần tử a và b
+        if (a.departmentID < b.departmentID) {
+            return -1;
+        }
+        if (a.departmentID > b.departmentID) {
+            return 1;
+        }
+        return 0;
     });
 }
 
@@ -299,6 +347,7 @@ const getListPosition = () => {
 const findValue = (pos, com) => {
     return datas.find(data => data.PositionName === pos && data.CompetenceID === com);
 }
+
 
 // render ra các thông số table
 const renderFigure = () => {
@@ -308,10 +357,10 @@ const renderFigure = () => {
         htmlFigure += `<div class='content-all-column' style="${i % 2 == 1 && 'background-color: #DBDEE7'}">`
         listPosition.forEach(item => {
             htmlFigure += `
-                <div class='competenceName' style='${!!findValue(item.name, comID.id) ?  'none' : 'pointer-events: none;'}'>
-                    <div data-code=${!!findValue(item.name, comID.id) ? 'none' : findValue(item.name, comID.id)} class='figure'>
-                        <div><input style="${i % 2 == 1 && 'background-color: #DBDEE7; border: 1px solid #DBDEE7'}" id='${item.name + '-' + comID.id}-min' onblur="updateData(event, '${item.name}', '${comID.id}', 'min')" type='number' min='1' value='${!!findValue(item.name, comID.id) ? (!!findValue(item.name, comID.id).CompetenceLevel && !!findValue(item.name, comID.id).CompetenceLevel ? findValue(item.name, comID.id).CompetenceLevel : '') : ''}'/></div>
-                        <div><input style="${i % 2 == 1 && 'background-color: #DBDEE7; border: 1px solid #DBDEE7'}" id='${item.name + '-' + comID.id}-max' onblur="updateData(event, '${item.name}', '${comID.id}', 'max')" type='number' min='1' value='${!!findValue(item.name, comID.id) ? (!!findValue(item.name, comID.id).CompetenceLevelMax && !!findValue(item.name, comID.id).CompetenceLevelMax ? findValue(item.name, comID.id).CompetenceLevelMax : '') : ''}'/></div>
+                <div class='competenceName' style='${!!findValue(item.positionName, comID.id) ? 'none' : 'pointer-events: none;'}'>
+                    <div class='figure'>
+                        <div><input style="${i % 2 == 1 && 'background-color: #DBDEE7; border: 1px solid #DBDEE7'}" id='${item.positionName + '-' + comID.id}-min' onblur="updateData(event, '${item.positionName}', '${comID.id}', 'min')" type='number' min='1' value='${!!findValue(item.positionName, comID.id) ? (!!findValue(item.positionName, comID.id).CompetenceLevel && !!findValue(item.positionName, comID.id).CompetenceLevel ? findValue(item.positionName, comID.id).CompetenceLevel : '') : ''}'/></div>
+                        <div><input style="${i % 2 == 1 && 'background-color: #DBDEE7; border: 1px solid #DBDEE7'}" id='${item.positionName + '-' + comID.id}-max' onblur="updateData(event, '${item.positionName}', '${comID.id}', 'max')" type='number' min='1' value='${!!findValue(item.positionName, comID.id) ? (!!findValue(item.positionName, comID.id).CompetenceLevelMax && !!findValue(item.positionName, comID.id).CompetenceLevelMax ? findValue(item.positionName, comID.id).CompetenceLevelMax : '') : ''}'/></div>
                     </div>
                 </div>`
         })
@@ -326,11 +375,18 @@ const renderCompetenceName = () => {
     let htmlListCompetenceID = ``
     listCompetence.forEach(item => {
         htmlListCompetenceID += `
-        <div class='competenceName'>
+        <div class='competenceName' style='position: relative'>
             <div class='figure-title'>${item.id}</div>
             <div style="display: flex;">
                 <span>Min</span>
                 <span>Max</span>
+            </div>
+            <div style='position: absolute; right: 3px; top: 3px'>
+            <svg width="12" height="12" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 0C2.40666 0 1.82664 0.175947 1.33329 0.505591C0.839943 0.835235 0.455426 1.30377 0.228363 1.85195C0.00129984 2.40013 -0.0581101 3.00333 0.0576455 3.58527C0.173401 4.16721 0.459123 4.70176 0.878681 5.12132C1.29824 5.54088 1.83279 5.8266 2.41473 5.94235C2.99667 6.05811 3.59987 5.9987 4.14805 5.77164C4.69623 5.54457 5.16476 5.16006 5.49441 4.66671C5.82405 4.17336 6 3.59334 6 3C5.99921 2.20459 5.68288 1.44199 5.12044 0.879557C4.55801 0.31712 3.79541 0.000794215 3 0V0ZM3 5.45456C2.51453 5.45456 2.03997 5.3106 1.63632 5.04089C1.23267 4.77118 0.918061 4.38783 0.732281 3.93932C0.546501 3.49081 0.497893 2.99728 0.592602 2.52114C0.687312 2.045 0.921087 1.60764 1.26436 1.26436C1.60764 0.921085 2.045 0.687311 2.52114 0.592601C2.99728 0.497891 3.49081 0.5465 3.93932 0.73228C4.38783 0.91806 4.77118 1.23267 5.04089 1.63632C5.31061 2.03997 5.45456 2.51453 5.45456 3C5.45412 3.65085 5.19537 4.27492 4.73514 4.73514C4.27492 5.19537 3.65085 5.45411 3 5.45456V5.45456Z" fill="#31ADFF"/>
+                <path d="M3 1.26392C2.92446 1.26392 2.85061 1.28632 2.7878 1.32828C2.72499 1.37025 2.67604 1.4299 2.64713 1.49969C2.61822 1.56948 2.61066 1.64628 2.6254 1.72037C2.64013 1.79445 2.67651 1.86251 2.72992 1.91592C2.78334 1.96934 2.85139 2.00571 2.92548 2.02045C2.99957 2.03519 3.07637 2.02763 3.14616 1.99872C3.21595 1.96981 3.2756 1.92086 3.31756 1.85805C3.35953 1.79524 3.38193 1.72139 3.38193 1.64585C3.38164 1.54465 3.3413 1.44767 3.26974 1.37611C3.19818 1.30455 3.1012 1.26421 3 1.26392V1.26392Z" fill="#31ADFF"/>
+                <path d="M3 2.47266C2.92408 2.47285 2.85132 2.5031 2.79763 2.55679C2.74395 2.61048 2.7137 2.68323 2.7135 2.75916V4.47759C2.7135 4.55355 2.74368 4.6264 2.79739 4.68011C2.8511 4.73382 2.92395 4.764 2.99991 4.764C3.07587 4.764 3.14872 4.73382 3.20243 4.68011C3.25614 4.6264 3.28631 4.55355 3.28631 4.47759V2.75916C3.28612 2.68326 3.25589 2.61054 3.20225 2.55685C3.1486 2.50317 3.07589 2.4729 3 2.47266V2.47266Z" fill="#31ADFF"/>
+                </svg>
             </div>
         </div>`;
     });
@@ -339,24 +395,39 @@ const renderCompetenceName = () => {
 
 // render ra các PositionName
 const renderPositionName = () => {
-    const columnPositionName = document.querySelector('.position-name-main');
-    let htmlListPositionName = 
-    `<div>
-    <div style='background-color: rgba(26, 102, 52, 0.2); border-bottom: 1px solid rgba(26, 102, 52, 0.3); position: relative' class='positionName'>
-        <div class="box-diagonal-line"></div>
-        <div style="position: absolute; top: 5px; right: 20px; font-weight: 600; color: #5A6276">Năng lực</div>
-        <div style="position: absolute; bottom: 5px; left: 20px; font-weight: 600; color: #5A6276">Chức danh</div>
-    </div>`;
-    listPosition.forEach(item => {
-        htmlListPositionName += `
-        <div class='positionName'>
-            <div style="display: flex; gap: 5px">
-                <div>${item.id}</div>
-                <div class="line-between-position"></div>
-                <div>${item.name}</div>
-            </div>
-        </div>`
+    let positionByDepartment = {};
+
+    // Phân nhóm Position theo Department
+    listPosition.forEach(position => {
+        if (!positionByDepartment[position.departmentID]) {
+            positionByDepartment[position.departmentID] = [];
+        }
+        positionByDepartment[position.departmentID].push(position);
     });
+
+    const columnPositionName = document.querySelector('.position-name-main');
+    let htmlListPositionName =
+        `<div>
+            <div style='background-color: rgba(26, 102, 52, 0.2); border-bottom: 1px solid rgba(26, 102, 52, 0.3); position: relative' class='positionName'>
+                <div class="box-diagonal-line"></div>
+                <div style="position: absolute; top: 5px; right: 20px; font-weight: 600; color: #5A6276">Năng lực</div>
+                <div style="position: absolute; bottom: 5px; left: 20px; font-weight: 600; color: #5A6276">Chức danh</div>
+            </div>`;
+    Object.entries(positionByDepartment).forEach((item) => {
+        htmlListPositionName += `<div style='position: relative; border: 1px solid black; width: 100%; right: 30%; padding-left: 67px'>
+        <div style='position: absolute; left: 10px; top: 50%; transform: translateY(-50%)'>${item[0]}</div>`
+
+        item[1].forEach(pos => {
+            htmlListPositionName += `<div class='positionName'>
+                <div style="display: flex; gap: 5px">
+                    <div>${pos.positionID}</div>
+                    <div class="line-between-position"></div>
+                    <div>${pos.positionName}</div>
+                </div>
+            </div>`
+        })
+        htmlListPositionName += `</div>`
+    })
     htmlListPositionName += `</div> 
         <div class="btn-add-ability" style='padding-left: 20px'>
             <span>
