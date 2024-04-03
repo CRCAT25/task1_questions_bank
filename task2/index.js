f8a825cf0fe1ad7108d68de109c595063b9fb4a0
doquocthanh <doquocthanh@github.com>
2024-04-03 14:26:22 +0700

    DanhGiaNangLuc - task2 - Add: Hoàn thiện giao diện và chức năng chỉnh sửa thông số

---
+++ task2/index.js
@@ -0,0 +1,426 @@
+const datas = [
+    {
+        "PositionID": "ACAD",
+        "PositionName": "Admin cửa hàng",
+        "DepartmentName": "Kế toán",
+        "CompetenceID": "add1",
+        "CompetenceName": "add1",
+        "CategoryName": "Cốt lõi",
+        "DepartmentID": "AC",
+        "CategoryID": "CL",
+        "Category": 3,
+        "Department": 8,
+        "Code": 10686,
+        "Framework": 10103,
+        "Competence": 13,
+        "Position": 40,
+        "CompetenceLevel": null,
+        "CompetenceLevelMax": null,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:58:03.67",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "BDGD",
+        "PositionName": "Giám đốc",
+        "DepartmentName": "Ban Giám đốc",
+        "CompetenceID": "add1",
+        "CompetenceName": "add1",
+        "CategoryName": "Cốt lõi",
+        "DepartmentID": "BD",
+        "CategoryID": "CL",
+        "Category": 3,
+        "Department": 2,
+        "Code": 10682,
+        "Framework": 10103,
+        "Competence": 13,
+        "Position": 1,
+        "CompetenceLevel": null,
+        "CompetenceLevelMax": null,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:57:52.233",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "LGWE",
+        "PositionName": "Nhân viên kho",
+        "DepartmentName": "Hội đồng thành viên",
+        "CompetenceID": "add1",
+        "CompetenceName": "add1",
+        "CategoryName": "Cốt lõi",
+        "DepartmentID": "BOM",
+        "CategoryID": "CL",
+        "Category": 3,
+        "Department": 1,
+        "Code": 10690,
+        "Framework": 10103,
+        "Competence": 13,
+        "Position": 53,
+        "CompetenceLevel": 3,
+        "CompetenceLevelMax": 3,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:58:06.39",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "ACAD",
+        "PositionName": "Admin cửa hàng",
+        "DepartmentName": "Kế toán",
+        "CompetenceID": "test1",
+        "CompetenceName": "năng lực 1",
+        "CategoryName": "Cốt lõi",
+        "DepartmentID": "AC",
+        "CategoryID": "CL",
+        "Category": 3,
+        "Department": 8,
+        "Code": 10684,
+        "Framework": 10103,
+        "Competence": 3,
+        "Position": 40,
+        "CompetenceLevel": 1,
+        "CompetenceLevelMax": 3,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:58:03.67",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "BDGD",
+        "PositionName": "Giám đốc",
+        "DepartmentName": "Ban Giám đốc",
+        "CompetenceID": "test1",
+        "CompetenceName": "năng lực 1",
+        "CategoryName": "Cốt lõi",
+        "DepartmentID": "BD",
+        "CategoryID": "CL",
+        "Category": 3,
+        "Department": 2,
+        "Code": 10680,
+        "Framework": 10103,
+        "Competence": 3,
+        "Position": 1,
+        "CompetenceLevel": null,
+        "CompetenceLevelMax": null,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:57:46.593",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "LGWE",
+        "PositionName": "Nhân viên kho",
+        "DepartmentName": "Hội đồng thành viên",
+        "CompetenceID": "test1",
+        "CompetenceName": "năng lực 1",
+        "CategoryName": "Cốt lõi",
+        "DepartmentID": "BOM",
+        "CategoryID": "CL",
+        "Category": 3,
+        "Department": 1,
+        "Code": 10688,
+        "Framework": 10103,
+        "Competence": 3,
+        "Position": 53,
+        "CompetenceLevel": 1,
+        "CompetenceLevelMax": 4,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:58:06.39",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "ACAD",
+        "PositionName": "Admin cửa hàng",
+        "DepartmentName": "Kế toán",
+        "CompetenceID": "Mô tả 1test",
+        "CompetenceName": "Mô tả 1test",
+        "CategoryName": "Năng lực bổ trợ",
+        "DepartmentID": "AC",
+        "CategoryID": "BT",
+        "Category": 4,
+        "Department": 8,
+        "Code": 10685,
+        "Framework": 10103,
+        "Competence": 5,
+        "Position": 40,
+        "CompetenceLevel": null,
+        "CompetenceLevelMax": null,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:58:03.67",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "BDGD",
+        "PositionName": "Giám đốc",
+        "DepartmentName": "Ban Giám đốc",
+        "CompetenceID": "Mô tả 1test",
+        "CompetenceName": "Mô tả 1test",
+        "CategoryName": "Năng lực bổ trợ",
+        "DepartmentID": "BD",
+        "CategoryID": "BT",
+        "Category": 4,
+        "Department": 2,
+        "Code": 10681,
+        "Framework": 10103,
+        "Competence": 5,
+        "Position": 1,
+        "CompetenceLevel": 3,
+        "CompetenceLevelMax": 5,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:57:48.53",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "LGWE",
+        "PositionName": "Nhân viên kho",
+        "DepartmentName": "Hội đồng thành viên",
+        "CompetenceID": "Mô tả 1test",
+        "CompetenceName": "Mô tả 1test",
+        "CategoryName": "Năng lực bổ trợ",
+        "DepartmentID": "BOM",
+        "CategoryID": "BT",
+        "Category": 4,
+        "Department": 1,
+        "Code": 10689,
+        "Framework": 10103,
+        "Competence": 5,
+        "Position": 53,
+        "CompetenceLevel": 2,
+        "CompetenceLevelMax": 5,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:58:06.39",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "ACAD",
+        "PositionName": "Admin cửa hàng",
+        "DepartmentName": "Kế toán",
+        "CompetenceID": "test3",
+        "CompetenceName": "năng lực 3",
+        "CategoryName": "Năng lực marketing",
+        "DepartmentID": "AC",
+        "CategoryID": "MA",
+        "Category": 5,
+        "Department": 8,
+        "Code": 10687,
+        "Framework": 10103,
+        "Competence": 8,
+        "Position": 40,
+        "CompetenceLevel": 1,
+        "CompetenceLevelMax": 1,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:58:03.67",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "BDGD",
+        "PositionName": "Giám đốc",
+        "DepartmentName": "Ban Giám đốc",
+        "CompetenceID": "test3",
+        "CompetenceName": "năng lực 3",
+        "CategoryName": "Năng lực marketing",
+        "DepartmentID": "BD",
+        "CategoryID": "MA",
+        "Category": 5,
+        "Department": 2,
+        "Code": 10683,
+        "Framework": 10103,
+        "Competence": 8,
+        "Position": 1,
+        "CompetenceLevel": null,
+        "CompetenceLevelMax": null,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:57:55.42",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    },
+    {
+        "PositionID": "LGWE",
+        "PositionName": "Nhân viên kho",
+        "DepartmentName": "Hội đồng thành viên",
+        "CompetenceID": "test3",
+        "CompetenceName": "năng lực 3",
+        "CategoryName": "Năng lực marketing",
+        "DepartmentID": "BOM",
+        "CategoryID": "MA",
+        "Category": 5,
+        "Department": 1,
+        "Code": 10691,
+        "Framework": 10103,
+        "Competence": 8,
+        "Position": 53,
+        "CompetenceLevel": null,
+        "CompetenceLevelMax": null,
+        "CreateBy": "Nguyễn Văn Hachi",
+        "CreateTime": "2024-03-29T16:58:06.39",
+        "LastModifiedBy": null,
+        "LastModifiedTime": null
+    }
+]
+
+// Khai báo tạo các list Competence và PositionName
+const listCompetence = [];
+const listPosition = [];
+
+// Lấy danh sách các Competence
+const getListCompetence = () => {
+    datas.forEach(data => {
+        const existingCompetence = listCompetence.find(item => item.id === data.CompetenceID);
+        if (!existingCompetence) {
+            listCompetence.push({
+                id: data.CompetenceID,
+                name: data.CompetenceName
+            });
+        }
+    });
+}
+
+// Lấy danh sách các Position
+const getListPosition = () => {
+    datas.forEach(data => {
+        const existingPosition = listPosition.find(item => item.id === data.PositionID);
+        if (!existingPosition) {
+            listPosition.push({
+                id: data.PositionID,
+                name: data.PositionName
+            });
+        }
+    });
+}
+
+// Tìm object trong datas
+const findValue = (pos, com) => {
+    return datas.find(data => data.PositionName === pos && data.CompetenceID === com);
+}
+
+// render ra các thông số table
+const renderFigure = () => {
+    const contentTable = document.querySelector('.table-content')
+    let htmlFigure = ``
+    listCompetence.forEach((comID, i) => {
+        htmlFigure += `<div class='content-all-column' style="${i % 2 == 1 && 'background-color: #DBDEE7'}">`
+        listPosition.forEach(item => {
+            htmlFigure += `
+                <div class='competenceName'>
+                    <div data-code=${findValue(item.name, comID.id).Code} class='figure'>
+                        <div><input style="${i % 2 == 1 && 'background-color: #DBDEE7; border: 1px solid #DBDEE7'}" id='${item.name + '-' + comID.id}-min' onblur="updateData(event, '${item.name}', '${comID.id}', 'min')" type='number' min='1' value='${!!findValue(item.name, comID.id) ? (!!findValue(item.name, comID.id).CompetenceLevel && !!findValue(item.name, comID.id) ? findValue(item.name, comID.id).CompetenceLevel : '') : ''}'/></div>
+                        <div><input style="${i % 2 == 1 && 'background-color: #DBDEE7; border: 1px solid #DBDEE7'}" id='${item.name + '-' + comID.id}-max' onblur="updateData(event, '${item.name}', '${comID.id}', 'max')" type='number' min='1' value='${!!findValue(item.name, comID.id) ? (!!findValue(item.name, comID.id).CompetenceLevelMax && !!findValue(item.name, comID.id) ? findValue(item.name, comID.id).CompetenceLevelMax : '') : ''}'/></div>
+                    </div>
+                </div>`
+        })
+        htmlFigure += `</div>`
+    });
+    contentTable.innerHTML = htmlFigure;
+}
+
+// render ra các CompetenceName
+const renderCompetenceName = () => {
+    const rowCompetenceName = document.querySelector('.tab-header');
+    let htmlListCompetenceID = ``
+    listCompetence.forEach(item => {
+        htmlListCompetenceID += `
+        <div class='competenceName'>
+            <div class='figure-title'>${item.id}</div>
+            <div style="display: flex;">
+                <span>Min</span>
+                <span>Max</span>
+            </div>
+        </div>`;
+    });
+    rowCompetenceName.innerHTML = htmlListCompetenceID;
+}
+
+// render ra các PositionName
+const renderPositionName = () => {
+    const columnPositionName = document.querySelector('.position-name-main');
+    let htmlListPositionName = 
+    `<div>
+    <div style='background-color: rgba(26, 102, 52, 0.2); border-bottom: 1px solid rgba(26, 102, 52, 0.3); position: relative' class='positionName'>
+        <div class="box-diagonal-line"></div>
+        <div style="position: absolute; top: 5px; right: 20px; font-weight: 600; color: #5A6276">Năng lực</div>
+        <div style="position: absolute; bottom: 5px; left: 20px; font-weight: 600; color: #5A6276">Chức danh</div>
+    </div>`;
+    listPosition.forEach(item => {
+        htmlListPositionName += `
+        <div class='positionName'>
+            <div style="display: flex; gap: 5px">
+                <div>${item.id}</div>
+                <div class="line-between-position"></div>
+                <div>${item.name}</div>
+            </div>
+        </div>`
+    });
+    htmlListPositionName += `</div> 
+        <div class="btn-add-ability" style='padding-left: 20px'>
+            <span>
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M19.5 10C19.5 15.2467 15.2467 19.5 10 19.5C4.75329 19.5 0.5 15.2467 0.5 10C0.5 4.7533 4.7533 0.5 10 0.5C15.2467 0.5 19.5 4.75329 19.5 10Z"
+                        stroke="#1A6634" />
+                    <path d="M10 5.71436V14.2858" stroke="#1A6634" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M5.71436 10.0001H14.2858" stroke="#1A6634" stroke-width="1.5"
+                        stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </span>
+            <span>Thêm chức danh</span>
+        </div>
+    `
+    columnPositionName.innerHTML = htmlListPositionName;
+}
+
+// update data Min Max
+const updateData = (event, pos, com, level) => {
+    let valueMin = document.getElementById(`${pos + '-' + com}-min`);
+    let valueMax = document.getElementById(`${pos + '-' + com}-max`);
+    const newValue = Number(event.target.value);
+    datas.forEach(data => {
+        if (data.PositionName === pos && data.CompetenceID === com) {
+            if (!(!!data.CompetenceLevel && !!data.CompetenceLevelMax) && newValue > 0) {
+                data.CompetenceLevel = newValue;
+                data.CompetenceLevelMax = newValue;
+                valueMin.value = newValue;
+                valueMax.value = newValue;
+                alert(`MIN và MAX của ID: ${data.Code} được thay đổi`);
+            }
+            if (level === "min" && data.CompetenceLevel !== newValue && typeof (newValue) === typeof (data.CompetenceLevel)) {
+                if (newValue > 0 && newValue <= data.CompetenceLevelMax) {
+                    data.CompetenceLevel = newValue;
+                    alert(`MIN của ID: ${data.Code} được thay đổi`);
+                }
+                else if (newValue > data.CompetenceLevelMax) {
+                    event.target.value = data.CompetenceLevelMax;
+                    data.CompetenceLevel = Number(event.target.value);
+                }
+                else if (newValue <= 0) {
+                    event.target.value = findValue(pos, com).CompetenceLevel;
+                    data.CompetenceLevel = findValue(pos, com).CompetenceLevel;
+                }
+            }
+            else if (level === "max" && data.CompetenceLevelMax !== newValue && typeof (newValue) === typeof (data.CompetenceLevel)) {
+                data.CompetenceLevelMax = newValue;
+                alert(`MAX của ID: ${data.Code} được thay đổi`);
+            }
+        }
+    });
+}
+
+// Thực hiện các hàm bên trong để render ra table
+const renderTableData = () => {
+    getListCompetence();
+    getListPosition();
+    renderCompetenceName();
+    renderPositionName();
+    renderFigure();
+}
+
+// Thực hiện render ra table
+renderTableData();
