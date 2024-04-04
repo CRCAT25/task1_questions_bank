67b756ee98568b78924c758ab85175b6207eb189
doquocthanh <doquocthanh@github.com>
2024-04-05 02:38:08 +0700

    DanhGiaNangLuc - index.js - Fix: Chỉnh giao diện phân nhóm và responsive

--- task2_danhgianangluc/index.js
+++ task2_danhgianangluc/index.js
@@ -298,6 +298,7 @@ const getListCompetence = () => {
         if (!existingCompetence) {
             listCompetence.push({
                 categoryID: data.CategoryID,
+                categoryName: data.CategoryName,
                 id: data.CompetenceID,
                 name: data.CompetenceName
             });
@@ -324,6 +325,7 @@ const getListPosition = () => {
         if (!existingPosition) {
             listPosition.push({
                 departmentID: data.DepartmentID,
+                departmentName: data.DepartmentName,
                 positionID: data.PositionID,
                 positionName: data.PositionName
             });
@@ -371,79 +373,102 @@ const renderFigure = () => {
 
 // render ra các CompetenceName
 const renderCompetenceName = () => {
+    let competenceByCategory = {};
+
+    // Phân nhóm Position theo Department
+    listCompetence.forEach(competence => {
+        if (!competenceByCategory[competence.categoryName]) {
+            competenceByCategory[competence.categoryName] = [];
+        }
+        competenceByCategory[competence.categoryName].push(competence);
+    });
+
     const rowCompetenceName = document.querySelector('.tab-header');
     let htmlListCompetenceID = ``
-    listCompetence.forEach(item => {
-        htmlListCompetenceID += `
-        <div class='competenceName' style='position: relative'>
-            <div class='figure-title'>${item.id}</div>
-            <div style="display: flex;">
-                <span>Min</span>
-                <span>Max</span>
+    Object.entries(competenceByCategory).forEach((item, index) => {
+        htmlListCompetenceID += `<div class='item-compe'>
+            <div class='compe' style='${index === Object.entries(competenceByCategory).length - 1 ? 'border-right: 1px solid black;' : ''}'>
+                <div class='compe-cate'>${item[0]}</div>
+                <div class='compe-list-ability'>`
+        item[1].forEach(com => {
+            htmlListCompetenceID += `
+            <div class='competenceName'>
+                <div class='figure-title'>${com.id}</div>
+                <div style="display: flex;">
+                    <span>Min</span>
+                    <span>Max</span>
+                </div>
             </div>
-            <div style='position: absolute; right: 3px; top: 3px'>
-            <svg width="12" height="12" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 0C2.40666 0 1.82664 0.175947 1.33329 0.505591C0.839943 0.835235 0.455426 1.30377 0.228363 1.85195C0.00129984 2.40013 -0.0581101 3.00333 0.0576455 3.58527C0.173401 4.16721 0.459123 4.70176 0.878681 5.12132C1.29824 5.54088 1.83279 5.8266 2.41473 5.94235C2.99667 6.05811 3.59987 5.9987 4.14805 5.77164C4.69623 5.54457 5.16476 5.16006 5.49441 4.66671C5.82405 4.17336 6 3.59334 6 3C5.99921 2.20459 5.68288 1.44199 5.12044 0.879557C4.55801 0.31712 3.79541 0.000794215 3 0V0ZM3 5.45456C2.51453 5.45456 2.03997 5.3106 1.63632 5.04089C1.23267 4.77118 0.918061 4.38783 0.732281 3.93932C0.546501 3.49081 0.497893 2.99728 0.592602 2.52114C0.687312 2.045 0.921087 1.60764 1.26436 1.26436C1.60764 0.921085 2.045 0.687311 2.52114 0.592601C2.99728 0.497891 3.49081 0.5465 3.93932 0.73228C4.38783 0.91806 4.77118 1.23267 5.04089 1.63632C5.31061 2.03997 5.45456 2.51453 5.45456 3C5.45412 3.65085 5.19537 4.27492 4.73514 4.73514C4.27492 5.19537 3.65085 5.45411 3 5.45456V5.45456Z" fill="#31ADFF"/>
-                <path d="M3 1.26392C2.92446 1.26392 2.85061 1.28632 2.7878 1.32828C2.72499 1.37025 2.67604 1.4299 2.64713 1.49969C2.61822 1.56948 2.61066 1.64628 2.6254 1.72037C2.64013 1.79445 2.67651 1.86251 2.72992 1.91592C2.78334 1.96934 2.85139 2.00571 2.92548 2.02045C2.99957 2.03519 3.07637 2.02763 3.14616 1.99872C3.21595 1.96981 3.2756 1.92086 3.31756 1.85805C3.35953 1.79524 3.38193 1.72139 3.38193 1.64585C3.38164 1.54465 3.3413 1.44767 3.26974 1.37611C3.19818 1.30455 3.1012 1.26421 3 1.26392V1.26392Z" fill="#31ADFF"/>
-                <path d="M3 2.47266C2.92408 2.47285 2.85132 2.5031 2.79763 2.55679C2.74395 2.61048 2.7137 2.68323 2.7135 2.75916V4.47759C2.7135 4.55355 2.74368 4.6264 2.79739 4.68011C2.8511 4.73382 2.92395 4.764 2.99991 4.764C3.07587 4.764 3.14872 4.73382 3.20243 4.68011C3.25614 4.6264 3.28631 4.55355 3.28631 4.47759V2.75916C3.28612 2.68326 3.25589 2.61054 3.20225 2.55685C3.1486 2.50317 3.07589 2.4729 3 2.47266V2.47266Z" fill="#31ADFF"/>
-                </svg>
-            </div>
-        </div>`;
+            `})
+        htmlListCompetenceID += `</div></div></div>`
     });
     rowCompetenceName.innerHTML = htmlListCompetenceID;
 }
 
 // render ra các PositionName
 const renderPositionName = () => {
-    let positionByDepartment = {};
+    let positionByDepartmentID = {};
 
     // Phân nhóm Position theo Department
     listPosition.forEach(position => {
-        if (!positionByDepartment[position.departmentID]) {
-            positionByDepartment[position.departmentID] = [];
+        if (!positionByDepartmentID[position.departmentID]) {
+            positionByDepartmentID[position.departmentID] = [];
         }
-        positionByDepartment[position.departmentID].push(position);
+        positionByDepartmentID[position.departmentID].push(position);
+        console.log(position.departmentName)
     });
+
+
+    console.log(positionByDepartmentID)
 
     const columnPositionName = document.querySelector('.position-name-main');
     let htmlListPositionName =
-        `<div>
-            <div style='background-color: rgba(26, 102, 52, 0.2); border-bottom: 1px solid rgba(26, 102, 52, 0.3); position: relative' class='positionName'>
+        `<div class='box-list-position'>
+            <div class='positionName box-divide-title'>
                 <div class="box-diagonal-line"></div>
-                <div style="position: absolute; top: 5px; right: 20px; font-weight: 600; color: #5A6276">Năng lực</div>
-                <div style="position: absolute; bottom: 5px; left: 20px; font-weight: 600; color: #5A6276">Chức danh</div>
-            </div>`;
-    Object.entries(positionByDepartment).forEach((item) => {
-        htmlListPositionName += `<div style='position: relative; border: 1px solid black; width: 100%; right: 30%; padding-left: 67px'>
-        <div style='position: absolute; left: 10px; top: 50%; transform: translateY(-50%)'>${item[0]}</div>`
+                <div class='title-nangluc'>Năng lực</div>
+                <div class='title-chucdanh'>Chức danh</div>
+            </div>
+            <div class='list-item-position'>
+            `;
 
+    Object.entries(positionByDepartmentID).forEach((item, index) => {
+        htmlListPositionName += `
+            <div class='item-position' style='${index === Object.entries(positionByDepartmentID).length - 1 ? 'border-bottom: 1px solid black;' : ''}'>
+                <div class='department-name' title='${datas.find(data => data.DepartmentID === item[0]).DepartmentName}'>${item[0]}</div>
+                <div class='positionName-particular'>
+        `
         item[1].forEach(pos => {
-            htmlListPositionName += `<div class='positionName'>
-                <div style="display: flex; gap: 5px">
-                    <div>${pos.positionID}</div>
-                    <div class="line-between-position"></div>
-                    <div>${pos.positionName}</div>
-                </div>
-            </div>`
+            htmlListPositionName += `
+                    <div class='positionName'>
+                        <div style="display: flex; gap: 5px">
+                            <div>${pos.positionID}</div>
+                            <div class="line-between-position"></div>
+                            <div>${pos.positionName}</div>
+                        </div>
+                    </div>
+            `
         })
-        htmlListPositionName += `</div>`
+        htmlListPositionName += `</div></div>`
     })
+    htmlListPositionName += `</div></div></div>`
+
     htmlListPositionName += `</div> 
-        <div class="btn-add-ability" style='padding-left: 20px'>
-            <span>
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M19.5 10C19.5 15.2467 15.2467 19.5 10 19.5C4.75329 19.5 0.5 15.2467 0.5 10C0.5 4.7533 4.7533 0.5 10 0.5C15.2467 0.5 19.5 4.75329 19.5 10Z"
-                        stroke="#1A6634" />
-                    <path d="M10 5.71436V14.2858" stroke="#1A6634" stroke-width="1.5" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                    <path d="M5.71436 10.0001H14.2858" stroke="#1A6634" stroke-width="1.5"
-                        stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-            </span>
-            <span>Thêm chức danh</span>
-        </div>
+    <div class="btn-add-ability btn-add-chucdanh">
+    <span>
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M19.5 10C19.5 15.2467 15.2467 19.5 10 19.5C4.75329 19.5 0.5 15.2467 0.5 10C0.5 4.7533 4.7533 0.5 10 0.5C15.2467 0.5 19.5 4.75329 19.5 10Z"
+                stroke="#1A6634" />
+            <path d="M10 5.71436V14.2858" stroke="#1A6634" stroke-width="1.5" stroke-linecap="round"
+                stroke-linejoin="round" />
+            <path d="M5.71436 10.0001H14.2858" stroke="#1A6634" stroke-width="1.5"
+                stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </span>
+    <span>Thêm chức danh</span>
+</div>
     `
     columnPositionName.innerHTML = htmlListPositionName;
 }
